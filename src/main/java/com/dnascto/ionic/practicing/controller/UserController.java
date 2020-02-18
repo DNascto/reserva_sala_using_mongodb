d7364741a53b46788004e3550938d947481c8c36
Daniel Nascimento <dgn.nascto@gmail.com>
2020-02-18 09:35:02 -0300

    removed mavem dependencies of spring security

--- src/main/java/com/dnascto/ionic/practicing/controller/UserController.java
+++ src/main/java/com/dnascto/ionic/practicing/controller/UserController.java
@@ -5,7 +5,7 @@ import com.dnascto.ionic.practicing.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin(origins = "http://localhost:8100")
 public class UserController {
     private UserService userService;
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
+//    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Autowired
     public UserController(UserService userService) {
